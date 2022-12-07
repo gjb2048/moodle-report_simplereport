8c08605989019058dafdea3da7fb372a27aedfd0
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-12-07 18:42:02 +0000

    Moodle 4.1 version.

--- version.php
+++ version.php
@@ -19,11 +19,16 @@
  *
  * @package   report_simplereport
  * @copyright 2020 Richard Jones {@link https://richardnz.net}
+ * @copyright &copy; 2022 G J Barnard.
+ * @author    G J Barnard - gjbarnard at gmail dot com and {@link http://moodle.org/user/profile.php?id=442195}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020030901;
-$plugin->requires  = 2017111300; // Minimum Moodle 3.4.
 $plugin->component = 'report_simplereport';
+$plugin->version = 2022120700;
+$plugin->requires  = 2022112800.00; // 4.1 (Build: 20221128).
+$plugin->supported = array(401, 401);
+$plugin->release = '401.1.0';
+$plugin->maturity = MATURITY_STABLE;
